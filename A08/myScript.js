3148b6a2d6811123f25c82ae228e087b03ceca2a
mlpinlac <mlpinlac@hawaii.edu>
2015-09-27 13:44:16 -1000

    A08
    A08 homework

---
+++ A08/myScript.js
@@ -0,0 +1,30 @@
+/**
+ * Created by michellelei on 9/26/15.
+ */
+
+$(document).ready(function () {
+
+  // initially hidden
+  $('dd').hide();
+
+  //on click, show or hide the answers
+  $("dt").click(function() {
+    $(this).next().slideToggle();
+
+    //Check if symbol is + or - and change it
+    if ($(this).children(".symbol").text() == " + ") {
+      $(this).children(".symbol").text(" - ");
+    }
+    else {
+      $(this).children(".symbol").text(" + ");
+    }
+  });
+});
+
+$(document).ready(function(){
+  $("#load").click(function(event) {
+    $('#loadcontent').load('partone.html');
+    var x = document.links.length;
+    document.getElementById("demo").innerHTML = x;
+  });
+});
